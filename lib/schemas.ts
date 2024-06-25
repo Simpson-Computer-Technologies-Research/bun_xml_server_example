713606a77b457e26d233edf7af21abf7fcf82b02
realTristan <heytristaann@gmail.com>
2024-06-25 10:51:36 -0400

    chore: update comment

--- lib/schemas.ts
+++ lib/schemas.ts
@@ -5,6 +5,10 @@ import { z } from "zod";
  * This is a Zod document type for XMLBuilderCreateOptions
  *
  * There must exist a root key in the object!
+ *
+ * This interface, in combination with: `z.instanceof(Object) as z.ZodType<XMLDocument>`
+ * (found below) is a good way to validate that the object is of type XMLDocument instead
+ * of manually creating a zod object with each key from the interface/type.
  */
 export interface XMLDocument extends XMLBuilderCreateOptions {
   root: Record<string, unknown>;
@@ -18,7 +22,6 @@ export const schemas = {
   http: {
     // schema for xml http request
     xml: {
-      // doc is of type XMLBuilderCreateOptions which can be any object
       doc: z.instanceof(Object) as z.ZodType<XMLDocument>,
     },
   },
